4136357b2ec55e056698b5beb7da0d98c94e812e
Sharan Athiyadath <sharanadhi@gmail.com>
2024-09-12 16:37:58 -0300

    integrated apis

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,53 @@
-// const apiKey = 'ghCH7E2rMd5WnA5Libu9znCOp0ObzGWN'; 
-// const baseUrl='https://api.giphy.com/v1/gifs/search';
-// const gifContainer = document.getElementById('gif-container');
+const apiKey = 'ghCH7E2rMd5WnA5Libu9znCOp0ObzGWN'; 
+const baseUrl='https://api.giphy.com/v1/gifs/search';
+const gifContainer = document.getElementById('gif-container');
 
 
-// async function getJokes(){
-//   const jokesData = await  axios.get('https://icanhazdadjoke.com/', {
-//     headers: { 'Accept': 'application/json' }
-//   });
-//   document.querySelector('.game__card--dad').textContent=jokesData.data.joke;
-//   emptyContainer(); 
-// }
+async function getJokes(){
+  const jokesData = await  axios.get('https://icanhazdadjoke.com/', {
+    headers: { 'Accept': 'application/json' }
+  });
+  document.querySelector('.game__card-back').textContent=jokesData.data.joke;
+  emptyContainer(); 
+}
 
-// const fetchGifs = async () => {
-//   const gifData = await axios.get(baseUrl, {
-//     params: {
-//       api_key: apiKey,
-//       q: 'funny reaction',
-//       limit: 20
-//     }
-//   });
-//   console.log(gifData);
-//   displayGifs(gifData.data.data)
-// };
 
-// function displayGifs(gifs){
-//   const randomNumbers = getRandomNumbers(4, 1, 19);
-//   emptyContainer();
-//   for(let i=0;i<randomNumbers.length;i++){
-//     let index=randomNumbers[i];
-//     const card = document.createElement('div');
-//     card.classList.add('game__card--gif');    
-//     const img = document.createElement('img');
-//     img.src = gifs[index].images.fixed_width.url;
-//     img.alt = gifs[index].title;
-//     img.classList.add('gif__img');
-//     card.appendChild(img);
-//     gifContainer.appendChild(card);
+const formElement = document.querySelector(".game__form");
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const query = form.gifSearch.value;
+  fetchGifs(query);
+});
+
+const fetchGifs = async (query) => {
+  const gifData = await axios.get(baseUrl, {
+    params: {
+      api_key: apiKey,
+      q: query,
+      limit: 1
+    }
+  });
+  console.log(gifData);
+  displayGifs(gifData.data.data)
+};
+
+function displayGifs(gifs){
+  // const randomNumbers = getRandomNumbers(4, 1, 19);
+  emptyContainer();
+  // for(let i=0;i<randomNumbers.length;i++){
+  //   let index=randomNumbers[i];
+    // const card = document.createElement('div');
+    // card.classList.add('game__card--gif');    
+    const img = document.createElement('img');
+    img.src = gifs[0].images.fixed_width.url;
+    img.alt = gifs[0].title;
+    img.classList.add('gif__img');
+    // card.appendChild(img);
+    gifContainer.appendChild(img);
     
-//   }
-// }
+  // }
+}
 
 
 
@@ -53,12 +62,12 @@
 //   return numbers;
 // }
 
-// function emptyContainer(){
-//   gifContainer.innerHTML='';
-// }
+function emptyContainer(){
+  gifContainer.innerHTML='';
+}
 
 // document.querySelector(".game__button").addEventListener("click", fetchGifs);
-// document.querySelector(".game__card--dad").addEventListener("click", getJokes);
+document.querySelector(".game__card").addEventListener("click", getJokes);
 
 const card = document.querySelector('.game__card');
 card.addEventListener('click', () => {
